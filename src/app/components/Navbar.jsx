22d24d8815e62f69151bd6d233f4cdd48ef353e7
somto <somto565@gmail.com>
2025-03-08 07:49:21 +0100

    `Added mobile menu functionality to Navbar component`

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,8 +1,20 @@
+'use client'
 import { assets } from '../../../assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Navbar = () => {
+
+    const sideMenuRef = useRef();
+
+    const openMenu = ()=> {
+        sideMenuRef.current.style.transform ='translateX(-16rem)'
+    }
+
+    const closeMenu = ()=> {
+        sideMenuRef.current.style.transform ='translateX(16rem)'
+    }
+
     return (
         <>
             <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
@@ -35,10 +47,25 @@ const Navbar = () => {
                         className='w-4'
                         alt='contact' /></a>
 
-                        <button className='block md:hidden ml-3 '>
+                        <button className='block md:hidden ml-3' onClick={openMenu}>
                         <Image src={assets.menu_black} alt='' className='w-6' />
                         </button>
                 </div>
+
+                {/* Mobile Menu */}
+                <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64
+                top-0 bottom bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+
+                <div className='absolute right-6  top-6' onClick={closeMenu}>
+                    <Image src={assets.close_black} alt='' className='w-5 cursor-pointer' />
+                </div>
+
+                    <li><a onClick={closeMenu} href='#top'>Home</a></li>
+                    <li><a onClick={closeMenu} href='#about'>About Me</a></li>
+                    <li><a onClick={closeMenu} href='#services'>Services</a></li>
+                    <li><a onClick={closeMenu} href='#work'>Blog</a></li>
+                    <li><a onClick={closeMenu} href='#contact'>Contact Me</a></li>
+                </ul>
             </nav>
         </>
     )
